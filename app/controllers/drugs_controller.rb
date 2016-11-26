e80a2f744194cfbd97cded7ef96dacd63a9ac0b8
Farruh <ufarruh@gmail.com>
2016-11-26 13:38:08 -0800

    Drug info

--- app/controllers/drugs_controller.rb
+++ app/controllers/drugs_controller.rb
@@ -1,6 +1,11 @@
 class DrugsController < ApplicationController
+
   def index
     @q = Drug.ransack(params[:q])
     @drugs = @q.result
+  end
+
+  def show
+    @drug = Drug.find(params[:id])
   end
 end
